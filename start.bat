516d9889d212fea8f5847437d381666e2b50b220
dao <806659381@qq.com>
2025-08-07 18:17:39 +0800

    update data [2025/08/07 周四 18:17:39.24]

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 "C:\Users\Dao\Software\zulu24.32.13-ca-jdk24.0.2-win_x64\bin\java.exe" -jar MinecraftStatsCLI.jar config.json
 git add .
-git commit -m "自动提交: 更新统计数据 [%date% %time%]"
+git commit -m "update data [%date% %time%]"
 git push
 pause
